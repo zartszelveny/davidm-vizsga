34a90ca342771920b10cf94e045b4e2408d35fa9
zartszelveny <118744071+zartszelveny@users.noreply.github.com>
2023-05-25 19:34:26 +0200

    7. feladat kész

--- src/main/java/hu/gde/runnersdemo/RunnerEntity.java
+++ src/main/java/hu/gde/runnersdemo/RunnerEntity.java
@@ -17,6 +17,8 @@ public class RunnerEntity {
     @OneToMany(mappedBy = "runner", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LapTimeEntity> laptimes = new ArrayList<>();
 
+    @ManyToOne
+    private SponsorEntity sponsor;
 
     public RunnerEntity() {
     }
---
+++ src/main/java/hu/gde/runnersdemo/SponsorEntity.java
@@ -0,0 +1,37 @@
+package hu.gde.runnersdemo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+    public class SponsorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long sponsorId;
+    @Column(nullable = false)
+    private String sponsorName;
+
+
+    @JsonIgnore
+    @OneToMany
+    @JoinColumn
+    private List<RunnerEntity> runners=new ArrayList<>();
+    public long getSponsorId() {
+        return sponsorId;
+    }
+
+    public void setSponsorId(long sponsorId) {
+        sponsorId = sponsorId;
+    }
+
+    public String getSponsorName() {
+        return sponsorName;
+    }
+
+    public void setSponsorName(String sponsorName) {
+        this.sponsorName = sponsorName;
+    }
+}
